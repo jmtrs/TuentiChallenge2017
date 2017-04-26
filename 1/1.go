3e942e08d1d7cc89f360016d26d81921d8ccf7da
jmtrs <jmtorres15@gmail.com>
2017-04-26 14:03:34 +0000

    jmtrs commit

---
+++ 1/1.go
@@ -0,0 +1,11 @@
+/* Problem 1 - PiZZA
+ * Author: jmtrs
+ * Languaje: GoLang
+ * Execution: go run TheBuffer.go < a.in > a.out
+ */
+
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world")
+}
