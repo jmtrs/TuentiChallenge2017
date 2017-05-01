96e79e04a4cf3e4da16b9f74ed8908fbeadbd8a5
jmtrs <jmtorres15@gmail.com>
2017-05-01 11:19:10 +0000

    jmtrs commit

--- 2/2.go
+++ 2/2.go
@@ -1,7 +1,7 @@
-/* Problem 1 - PiZZA
+/* Problem 2 - Bowling
  * Author: jmtrs
  * Languaje: GoLang
- * Execution: go run 1.go < a.in > a.out
+ * Execution: go run 2.go < a.in > a.out
  */
 
 package main
@@ -21,6 +21,7 @@ func init() {
 }
 
 func main() {
+	var cases int
 	var people int
 	var portions string
 	var portionsTotal int
@@ -30,7 +31,7 @@ func main() {
 	var count int
 
 	// Open an input file, exit on error.
-	inputFile, err := os.Open("a2.in")
+	inputFile, err := os.Open("a.in")
 	if err != nil {
 		log.Fatal("Error opening input file:", err)
 	}
@@ -47,12 +48,17 @@ func main() {
 	for scanner.Scan() {
 		counter++
 		if counter == 1 {
+			cases, err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println(err)
+			}
 
 		} else if counter%2 == 0 {
 			people, err = strconv.Atoi(scanner.Text())
 			if err != nil {
 				fmt.Println(err)
 			}
+			//fmt.Printf("people: %d\n", people)
 
 		} else {
 			portions = scanner.Text()
@@ -64,8 +70,10 @@ func main() {
 			for i := 0; i < len(portions); i++ {
 				aux, err = strconv.Atoi(portions[i])
 				if err != nil {
+					//proper err handling
 				}
 				portionsTotal += aux
+				//fmt.Println(aux)
 
 				if i == people-1 {
 					pizzas = float64(portionsTotal) / 8
@@ -74,13 +82,21 @@ func main() {
 					}
 					pizzasdef = int(pizzas)
 					count++
-					fmt.Printf("Case #%d: %d\n", count, pizzasdef)
+					fmt.Printf("case %d: %d\n", count, pizzasdef)
 					aux = 0
 					portionsTotal = 0
 				}
 			}
+
+			//fmt.Printf("portions: %s\n", portions)
+
 		}
+
+		//fmt.Println(scanner.Text())
+		//fmt.Printf("%d\n", counter)
 	}
+
+	//fmt.Printf("casos: %d", cases)
 
 	// When finished scanning if any error other than io.EOF occured
 	// it will be returned by scanner.Err().
